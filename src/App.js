be5655b86ad628f692a6fc7f1eec9ad841520851
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2022-11-29 20:55:33 -0500

    implemented delete modal

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import Auth from "./user/pages/Auth";
 import Users from "./user/pages/Users";
 
 const App = () => {
@@ -29,6 +30,9 @@ const App = () => {
           </Route>
           <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
           </Route>
           <Redirect to="/" />
         </Switch>
