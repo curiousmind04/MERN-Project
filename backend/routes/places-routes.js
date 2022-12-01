d8989463aae63a25641ad856d4126aa377be9ea4
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2022-12-01 16:21:06 -0500

    added place specific routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,28 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Request in Places");
-  res.json({ message: "It works!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous skyscrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json({ place });
 });
 
 module.exports = router;
