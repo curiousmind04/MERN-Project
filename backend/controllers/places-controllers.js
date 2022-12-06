075cd4f32f41fc8d261f46a76f9244731169d7cb
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2022-12-05 19:54:26 -0500

    creating places and adding it to a user

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,9 +1,11 @@
 const uuid = require("uuid");
 const { validationResult } = require("express-validator");
+const mongoose = require("mongoose");
 
 const HttpError = require("../models/http-error");
 const getCoordsForAddress = require("../util/location");
 const Place = require("../models/place");
+const User = require("../models/user");
 
 let DUMMY_PLACES = [
   {
@@ -96,8 +98,31 @@ const createPlace = async (req, res, next) => {
     creator,
   });
 
+  let user;
   try {
-    await createdPlace.save();
+    user = await User.findById(creator);
+  } catch (err) {
+    const error = new HttpError(
+      "Creating place failed, please try again.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!user) {
+    const error = new HttpError("Could not find user for provided id.", 404);
+    return next(error);
+  }
+
+  console.log(user);
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await createdPlace.save({ session: sess });
+    user.places.push(createdPlace);
+    await user.save({ session: sess });
+    await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError("Creating place failed, please try again", 500);
     return next(error);
@@ -186,4 +211,11 @@ exports.deletePlace = deletePlace;
 //       "lat": 40.7063069,
 //       "lng": -74.010329
 //   }
+// }
+
+// {
+//   "title": "Empire State Building",
+//   "description": "A very famous building.",
+//   "creator": "638e8c9f2d0c2c9b00488f41",
+//   "address": "20 W 34th St, New York, NY 10001"
 // }
