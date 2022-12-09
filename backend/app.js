a3ea06239aa288231f24f3996e30bce89d5e1001
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2022-12-09 16:14:52 -0500

    changed start script and port

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.DB_URL)
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((err) => {
     console.log(err);
